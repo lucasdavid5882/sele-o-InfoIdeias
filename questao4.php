0f83d1ca484240cf56a7f37f52aaa4d569a29c83
Lucas David <lucasdavid5882@gmail.com>
2021-12-15 15:51:24 -0300

    alterado nome de funcao na questao4

--- questao4.php
+++ questao4.php
@@ -1,5 +1,5 @@
 <?php
-function check($arr){
+function SEQUENCIACRESCENTE($arr){
     $n = count($arr);
     $count = 0;
     $index = -1;
@@ -44,25 +44,25 @@ function check($arr){
 }
   
   
-echo check([1, 3, 2, 1]);
-echo check([1, 3, 2]);
-echo check([1, 2, 1, 2]);
-echo check([3, 6, 5, 8, 10, 20, 15]);
-echo check([1, 1, 2, 3, 4, 4]);
-echo check([1, 4, 10, 4, 2]);
-echo check([10, 1, 2, 3, 4, 5]);
-echo check([1, 1, 1, 2, 3]);
-echo check([0, -2, 5, 6]);
-echo check([1, 2, 3, 4, 5, 3, 5, 6]);
-echo check([40, 50, 60, 10, 20, 30]);
-echo check([1, 1]);
-echo check([1, 2, 5, 3, 5]);
-echo check([1, 2, 5, 5, 5]);
-echo check([10, 1, 2, 3, 4, 5, 6, 1]);
-echo check([1, 2, 3, 4, 3, 6]);
-echo check([1, 2, 3, 4, 99, 5, 6]);
-echo check([123, -17, -5, 1, 2, 3, 12, 43, 45]);
-echo check([3, 5, 67, 98, 3]);
+echo SEQUENCIACRESCENTE([1, 3, 2, 1]);
+echo SEQUENCIACRESCENTE([1, 3, 2]);
+echo SEQUENCIACRESCENTE([1, 2, 1, 2]);
+echo SEQUENCIACRESCENTE([3, 6, 5, 8, 10, 20, 15]);
+echo SEQUENCIACRESCENTE([1, 1, 2, 3, 4, 4]);
+echo SEQUENCIACRESCENTE([1, 4, 10, 4, 2]);
+echo SEQUENCIACRESCENTE([10, 1, 2, 3, 4, 5]);
+echo SEQUENCIACRESCENTE([1, 1, 1, 2, 3]);
+echo SEQUENCIACRESCENTE([0, -2, 5, 6]);
+echo SEQUENCIACRESCENTE([1, 2, 3, 4, 5, 3, 5, 6]);
+echo SEQUENCIACRESCENTE([40, 50, 60, 10, 20, 30]);
+echo SEQUENCIACRESCENTE([1, 1]);
+echo SEQUENCIACRESCENTE([1, 2, 5, 3, 5]);
+echo SEQUENCIACRESCENTE([1, 2, 5, 5, 5]);
+echo SEQUENCIACRESCENTE([10, 1, 2, 3, 4, 5, 6, 1]);
+echo SEQUENCIACRESCENTE([1, 2, 3, 4, 3, 6]);
+echo SEQUENCIACRESCENTE([1, 2, 3, 4, 99, 5, 6]);
+echo SEQUENCIACRESCENTE([123, -17, -5, 1, 2, 3, 12, 43, 45]);
+echo SEQUENCIACRESCENTE([3, 5, 67, 98, 3]);
 
 
 
